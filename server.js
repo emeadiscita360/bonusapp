d92806252d309403bf536787861c073d2959e3b3
las-zlo <132357993+las-zlo@users.noreply.github.com>
2024-09-19 16:17:22 +0200

    Organized project structure and added public directory

--- server.js
+++ server.js
@@ -1,21 +1,25 @@
-const express = require('express');  // Import the express library
-const axios = require('axios');  // Import axios to make HTTP requests
+const express = require('express');
+const axios = require('axios');
+const path = require('path');
 const app = express();
 
-app.use(express.json());  // Middleware to parse JSON request bodies
+// Middleware to serve static files from 'public' directory
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(express.json());
 
 // Replace with your Azure AD credentials
-const clientId = 'b8c22525-9f28-49dd-a7ae-6e62e83ddac3';  // Your Azure AD client ID
-const clientSecret = '9Px8Q~ukGIuLajO-n1E.A44o5nyDl6IDaP_P6bha';  // Your Azure AD client secret (store securely)
-const tenantId = 'eb06985d-06ca-4a17-81da-629ab99f6505';  // Your Azure AD tenant ID
-const resource = 'https://management.azure.com/.default';  // The scope for your Logic App
+const clientId = 'b8c22525-9f28-49dd-a7ae-6e62e83ddac3';
+const clientSecret = '9Px8Q~ukGIuLajO-n1E.A44o5nyDl6IDaP_P6bha';
+const tenantId = 'eb06985d-06ca-4a17-81da-629ab99f6505';
+const resource = 'https://management.azure.com/.default';
 
 // Endpoint to generate the Bearer token
 app.post('/api/get-token', async (req, res) => {
     try {
         const tokenResponse = await axios.post(`https://login.microsoftonline.com/${tenantId}/oauth2/v2.0/token`, new URLSearchParams({
             client_id: clientId,
-            client_secret: clientSecret,  // Use the new client secret
+            client_secret: clientSecret,
             grant_type: 'client_credentials',
             scope: resource
         }), {
