29c3325402acd555b14b9b5f297e073e0b19bc2f
emeadiscita360 <emeadiscita360@emerson.com>
2024-09-23 09:48:58 +0200

    server.js update

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 // Replace with your Azure AD credentials
 const clientId = 'b8c22525-9f28-49dd-a7ae-6e62e83ddac3';
 const clientSecret = '9Px8Q~ukGIuLajO-n1E.A44o5nyDl6IDaP_P6bha';
-const tenantId = 'Yeb06985d-06ca-4a17-81da-629ab99f6505';
+const tenantId = 'eb06985d-06ca-4a17-81da-629ab99f6505';
 const resource = 'https://management.azure.com/.default';
 
 // Endpoint to generate the Bearer token
